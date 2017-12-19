33ef8bbcf34fdcb226048e94c85277e31bea8762
masoo <masoo@iscte-iul.pt>
2017-12-19 17:12:01 +0000

    Este commit tem como novos funcionalidades adicionar funcionalidades a interface gráfica, adicionar dados dos ficheiros á interface gráfica, criar funcionalidade para geração aleatória de pesos

--- src/antiSpamFilter/GUI.java
+++ src/antiSpamFilter/GUI.java
@@ -1,5 +1,8 @@
 /* 
- * 1º Commit
+ * This is the version that contains the GUI but without any action listeners 
+ * development. But in the mean time, it has been developed a frame that contains 
+ * JTabbedPane to alternate between the manual input table and the automatic input
+ * version.
  */
 package antiSpamFilter;
 
@@ -14,34 +17,46 @@ import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.DefaultTableModel;
 
 public class GUI {
 
 	private JFrame frame = new JFrame("AntiSpamFilter");
+	private JTabbedPane tabs = new JTabbedPane();
 	private JPanel nPanel = new JPanel();
-	private JPanel cPanel = new JPanel();
-	private DefaultTableModel tableModel;
-	private JTable table = new JTable(tableModel);
-	private JScrollPane scroll = new JScrollPane();
+	private JPanel editablePanel = new JPanel();
+	private JPanel nonEditablePanel = new JPanel();
+	private JPanel sPanel = new JPanel();
+	private DefaultTableModel editable;
+	private DefaultTableModel nonEditable;
+	private JTable editableTable = new JTable(editable);
+	private JTable nonEditableTable = new JTable(nonEditable);
+	private JScrollPane editableScroll = new JScrollPane();
+	private JScrollPane nonEditableScroll = new JScrollPane();
 	private ArrayList<String> rules = new ArrayList<String>();
 	private JLabel labelPathRules = new JLabel("Path rules");
 	private JLabel labelPathSpam = new JLabel("Path spam");
 	private JLabel labelPathHam = new JLabel("Path ham");
+	private JButton generate = new JButton("Auto Config");
 	private JTextField pathRules = new JTextField("rules.cf");
 	private JTextField pathSpam = new JTextField("spam.log");
 	private JTextField pathHam = new JTextField("ham.log");
+	private JButton calculate = new JButton("Calculate");
 
 	public static void main(String[] args) {
 		try {
@@ -77,7 +92,7 @@ public class GUI {
 	private void addFrameContent() {
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setLayout(new BorderLayout());
-		frame.setPreferredSize(new Dimension(600, 500));
+		frame.setPreferredSize(new Dimension(520, 575));
 		frame.pack();
 
 		pathRules.setColumns(5);
@@ -94,7 +109,7 @@ public class GUI {
 		nPanel.add(labelPathHam);
 		nPanel.add(pathHam);
 
-		tableModel = new DefaultTableModel() {
+		editable = new DefaultTableModel() {
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				switch (column) {
@@ -107,17 +122,111 @@ public class GUI {
 			}
 		};
 
-		tableModel.setColumnIdentifiers(new Object[] { "RULES", "WEIGHTS" });
+		nonEditable = new DefaultTableModel() {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
 
-		table.setModel(tableModel);
+		editable.setColumnIdentifiers(new Object[] { "RULES", "WEIGHTS" });
+		nonEditable.setColumnIdentifiers(new Object[] { "RULES", "WEIGHTS" });
+		for (String rule : rules) {
+			editable.addRow(new Object[] { rule, 0 });
+			nonEditable.addRow(new Object[] { rule, 0 });
+		}
 
-		scroll.add(table);
-		scroll.setViewportView(table);
+		editableTable.setModel(editable);
+		nonEditableTable.setModel(nonEditable);
 
-		cPanel.add(scroll);
+		editableScroll.add(editableTable);
+		editableScroll.setViewportView(editableTable);
+		nonEditableScroll.add(nonEditableTable);
+		nonEditableScroll.setViewportView(nonEditableTable);
+
+		editablePanel.add(editableScroll);
+		nonEditablePanel.add(nonEditableScroll);
+
+		tabs.add("Automatic Input Table", nonEditablePanel);
+		tabs.add("Manual Input Table", editablePanel);
+		
+		tabs.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent arg0) {
+				tabbedChangedAction(arg0);
+			}
+		});
+		
+		generate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				generateAction();
+			}
+		});
+
+		calculate.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				calculateAction();
+			}
+		});
+
+		sPanel.add(generate);
+		sPanel.add(calculate);
 
 		frame.add(nPanel, BorderLayout.NORTH);
-		frame.add(cPanel, BorderLayout.CENTER);
+		frame.add(tabs, BorderLayout.CENTER);
+		frame.add(sPanel, BorderLayout.SOUTH);
+	}
+	
+	private void tabbedChangedAction(ChangeEvent e) {
+		SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				JTabbedPane pane = (JTabbedPane) e.getSource();
+				if (pane.getSelectedIndex() == 1)
+					generate.setText("Random Config");
+				else
+					generate.setText("Auto Config");
+				return null;
+			}
+		};
+		worker.execute();
+	}	
+
+	private void generateAction() {
+		SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				if (tabs.getSelectedIndex() == 0) {
+					generate.setEnabled(false);
+					System.out.println("It is suposed to process the weights");
+					Thread.currentThread().sleep(3000);
+					generate.setEnabled(true);
+				} else {
+					for (String str: rules) {
+						double weight = ((int) (Math.random() * (5.0 - -5.0))) + -5.0;
+						editable.setValueAt(weight, rules.indexOf(str), 1);
+					}
+				}
+				return null;
+			}
+		};
+		worker.execute();
+	}
+
+	private void calculateAction() {
+		SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				calculate.setEnabled(false);
+				System.out.println("Calculate FP &b FN using current table");
+				Thread.currentThread().sleep(3000);
+				calculate.setEnabled(true);
+				return null;
+			}
+		};
+		worker.execute();
 	}
 
 }
